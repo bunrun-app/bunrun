ae8e202905960d3157ff4c8629556e675d6320d7
Dorin Flocos <dflocos@gmail.com>
2025-01-30 12:28:26 -0500

    Initial Commit

---
+++ bunrun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bunrun
+//
+//  Created by dorin flocos on 1/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bunrun/bunrunApp.swift
@@ -0,0 +1,17 @@
+//
+//  bunrunApp.swift
+//  bunrun
+//
+//  Created by dorin flocos on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct bunrunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
